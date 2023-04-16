fe6257102452d8feba702da7dd71f6b8d593bef9
bilalanees98 <bilalanees1998@gmail.com>
2023-04-16 14:17:22 +0500

    add: typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,49 @@
+const colors = {
+  "primary-one": "#303030",
+  "primary-two": "#F5F5F5",
+  "secondary-one": "#606A8B",
+  "tertiary-one": "#A9A9A9",
+};
+const fontFamily = {
+  jost: ["Jost", "-apple-system", "Helvetica Neue", "Helvetica", "sans-serif"],
+  montserrat: [
+    "Montserrat",
+    "-apple-system",
+    "Helvetica Neue",
+    "Helvetica",
+    "sans-serif",
+  ],
+};
+
+const fontSize = {
+  s: "0.5rem",
+  "2s": "1rem",
+  "3s": "1.125rem",
+  m: "1.25rem",
+  "2m": "1.5rem",
+  l: "1.75rem",
+};
+
+const screens = {
+  mobile: "320px",
+  // => @media (min-width: 320px) { ... }
+
+  tablet: "480px",
+  // => @media (min-width: 480px) { ... }
+
+  laptop: "1024px",
+  // => @media (min-width: 1024px) { ... }
+};
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    fontFamily,
+    fontSize,
+    screens,
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 };

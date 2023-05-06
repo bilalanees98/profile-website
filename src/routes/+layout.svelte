993a6ad6c59de45b9a0a73717933f62e06786be7
bilalanees98 <bilalanees1998@gmail.com>
2023-05-06 16:07:32 +0500

    add: nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../app.css";
+  import Nav from "./Nav.svelte";
 </script>
 
+<Nav />
 <div
   class="h-screen flex flex-col grow font-montserrat text-primary-two bg-primary-one"
 >
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,11 @@
+<div
+  class="flex flex-row justify-between bg-primary-one font-montserrat text-primary-two border-b border-primary-one pb-5 pt-16 px-24 items-center"
+>
+  <div class="">
+    <img src="/graphics/logo.svg" alt="" />
+  </div>
+  <div class="text-m">
+    <a href="/">About</a>
+    <a href="/projects" class="pl-[3.25rem]">Projects</a>
+  </div>
+</div>

a76983e73cfaadd682d22c4caadb9bdbb1d43ce1
bilalanees98 <bilalanees1998@gmail.com>
2023-06-28 17:40:35 +0500

    add: footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
   import "../app.css";
+  import Footer from "./Footer.svelte";
   import Nav from "./Nav.svelte";
 </script>
 
 <Nav />
 <div
-  class="h-screen flex flex-col grow font-montserrat text-primary-two bg-primary-one"
+  class="h-[77vh] flex flex-col grow font-montserrat text-primary-two bg-primary-one"
 >
   <slot />
 </div>
+<Footer />
